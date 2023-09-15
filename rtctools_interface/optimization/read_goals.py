26ed0c506ef6091e2434ce53bd5d166bb7ef9e92
Joris <joris.bentvelsen@vortech.nl>
2023-09-15 09:34:48 +0200

    Move goal checks to seperate function

--- rtctools_interface/optimization/read_goals.py
+++ rtctools_interface/optimization/read_goals.py
@@ -12,23 +12,31 @@ from rtctools_interface.optimization.goal_table_schema import (
 )
 
 
+def goal_table_checks(goal_table):
+    """Validate input goal table."""
+    if "goal_type" not in goal_table:
+        raise ValueError("Goal type column not in goal table.")
+    if "active" not in goal_table:
+        raise ValueError("Active column not in goal table.")
+    for _, row in goal_table.iterrows():
+        if row["goal_type"] not in GOAL_TYPES.keys():
+            raise ValueError(f"Goal of type {row['goal_type']} is not allowed. Allowed are {GOAL_TYPES.keys()}")
+        if int(row["active"]) not in [0, 1]:
+            raise ValueError("Value in active column should be either 0 or 1.")
+
+
 def get_goals_from_csv(
     file,
 ) -> dict[str, List[Union[RangeGoalModel, RangeRateOfChangeGoalModel, MinMaximizationGoalModel]]]:
     """Read goals from csv file and check values"""
     raw_goal_table = pd.read_csv(file, sep=",")
-    if "goal_type" not in raw_goal_table:
-        raise ValueError("Goal type column not in goal table.")
-    if "active" not in raw_goal_table:
-        raise ValueError("Active column not in goal table.")
+    goal_table_checks(raw_goal_table)
+
     parsed_goals = {goal_type: [] for goal_type in GOAL_TYPES.keys()}
     for _, row in raw_goal_table.iterrows():
-        if row["goal_type"] not in GOAL_TYPES.keys():
-            raise ValueError(f"Goal of type {row['goal_type']} is not allowed. Allowed are {GOAL_TYPES.keys()}")
         if int(row["active"]) == 1:
             parsed_goals[row["goal_type"]].append(GOAL_TYPES[row["goal_type"]](**row))
-        elif int(row["active"]) != 0:
-            raise ValueError("Value in active column should be either 0 or 1.")
+
     # TODO: Make a pydantic model for the set of all goals and do the following validation there.
     ids = [parsed_goal.goal_id for goal_by_type in parsed_goals.values() for parsed_goal in goal_by_type]
     if len(ids) != len(set(ids)):

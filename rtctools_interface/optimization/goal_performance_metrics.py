d678a18d51e8edc4c6f83965ab75b2df19347800
Joris <joris.bentvelsen@vortech.nl>
2023-10-16 17:20:08 +0200

    Add statistics for rate range of change goal
    For now, no statistics on target-exceedance yet. This is a bit more involved, as the targets are percentages while the timeseries are not.

--- rtctools_interface/optimization/goal_performance_metrics.py
+++ rtctools_interface/optimization/goal_performance_metrics.py
@@ -10,6 +10,7 @@ from rtctools_interface.optimization.goal_table_schema import (
     MaximizationGoalModel,
     MinimizationGoalModel,
     RangeGoalModel,
+    RangeRateOfChangeGoalModel,
 )
 from rtctools_interface.optimization.type_definitions import TargetDict
 
@@ -19,6 +20,24 @@ logger = logging.getLogger("rtctools")
 ABS_TOL = 0.001
 
 
+def get_mean_absolute_percentual_difference(timeseries):
+    """Calculate the mean absolute percentual difference."""
+    differences = np.diff(timeseries)
+    mapd = np.mean(np.abs(differences / timeseries[:-1]))
+    return mapd
+
+
+def get_absolute_sum_difference(timeseries):
+    """Calculate the mean of absolute first-order difference."""
+    mad = np.mean(np.abs(np.diff(timeseries)))
+    return mad
+
+
+def get_max_difference(timeseries):
+    """Get maximum one step difference"""
+    return max(np.diff(timeseries))
+
+
 def get_basic_metrics(timeseries: np.array):
     """Get general metrics applicable for each goal type."""
     metrics = {
@@ -26,6 +45,9 @@ def get_basic_metrics(timeseries: np.array):
         "timeseries_min": min(timeseries),
         "timeseries_max": max(timeseries),
         "timeseries_avg": np.mean(timeseries),
+        "mean_absolute_percentual_difference": get_mean_absolute_percentual_difference(timeseries),
+        "mean_absolute_difference": get_absolute_sum_difference(timeseries),
+        "max_difference": get_max_difference(timeseries),
     }
     return metrics
 
@@ -65,12 +87,21 @@ def performance_metrics_range(results: Dict[str, np.array], goal: RangeGoalModel
     return pd.Series(metrics)
 
 
+def performance_metrics_rangerateofchange(results: Dict[str, np.array], goal: RangeGoalModel, _targets):
+    """Get all relevant statistics for a range-rate-of-change goal."""
+    metrics = {}
+    state_timeseries = results[goal.state]
+    metrics = metrics | get_basic_metrics(state_timeseries)
+    return pd.Series(metrics)
+
+
 def get_performance_metrics(results, goal: BaseGoalModel, targets: TargetDict):
     """Returns a dict with performance metrics for each goal."""
     if type(goal) in [MinimizationGoalModel, MaximizationGoalModel]:  # pylint: disable=unidiomatic-typecheck
         return performance_metrics_minmaximization(results, goal)
-
     if type(goal) in [RangeGoalModel]:
         return performance_metrics_range(results, goal, targets)
+    if type(goal) in [RangeRateOfChangeGoalModel]:
+        return performance_metrics_rangerateofchange(results, goal, targets)
     logger.info("No performance metrics are implemented for goal of type: %s", str(type(goal)))
     return None

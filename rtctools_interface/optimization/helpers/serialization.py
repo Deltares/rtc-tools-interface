30f0bf24f81acaaa05a5beaab733e0464ebbe9a3
Joris <joris.bentvelsen@vortech.nl>
2023-11-13 15:03:41 +0100

    Extend functionality of serialization

--- rtctools_interface/optimization/helpers/serialization.py
+++ rtctools_interface/optimization/helpers/serialization.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Any
 
 import numpy as np
+import pandas as pd
 from rtctools_interface.optimization.plot_and_goal_schema import GOAL_TYPE_COMBINED_MODEL
 from rtctools_interface.optimization.plot_table_schema import PlotTableRow
 
@@ -19,6 +20,8 @@ def custom_encoder(obj):
         return {"__type__": "ndarray", "data": obj.tolist()}
     if isinstance(obj, Path):
         return {"__type__": "path", "value": str(obj)}
+    if isinstance(obj, pd.DataFrame):
+        return {"__type__": "pandas_dataframe", "value": obj.to_json()}
     if hasattr(obj, "dict"):
         return obj.dict()
     raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
@@ -31,22 +34,24 @@ def serialize(plot_data_and_config: PlotDataAndConfig) -> str:
 
 def custom_decoder(dct: Any) -> Any:
     """Custom JSON decoder for types not supported by default."""
+    # pylint: disable=too-many-return-statements
     if isinstance(dct, dict):
         for key, value in dct.items():
             dct[key] = custom_decoder(value)  # Recursively process each value
         if dct.get("__type__") == "datetime":
             return datetime.datetime.fromisoformat(dct["value"])
-        elif dct.get("__type__") == "date":
+        if dct.get("__type__") == "date":
             return datetime.date.fromisoformat(dct["value"])
-        elif dct.get("__type__") == "ndarray":
+        if dct.get("__type__") == "ndarray":
             return np.array(dct["data"])
-        elif dct.get("__type__") == "path" and dct["value"]:
+        if dct.get("__type__") == "path" and dct["value"]:
             return Path(dct["value"])
+        if dct.get("__type__") == "pandas_dataframe":
+            return pd.read_json(dct["value"])
         return dct
-    elif isinstance(dct, list):
+    if isinstance(dct, list):
         return [custom_decoder(item) for item in dct]  # Recursively process each item in the list
-    else:
-        return dct
+    return dct
 
 
 def deserialize(serialized_str: str) -> PlotDataAndConfig:
@@ -54,14 +59,15 @@ def deserialize(serialized_str: str) -> PlotDataAndConfig:
     data = json.loads(serialized_str, object_hook=custom_decoder)
 
     # Reconstruct pydantic models
-    goal_generator_goals = [
-        GOAL_TYPE_COMBINED_MODEL[item["goal_type"]](**item)
-        for item in data["plot_options"]["plot_config"]
-        if item["specified_in"] == "goal_generator"
-    ]
-    python_goals = [
-        PlotTableRow(**item) for item in data["plot_options"]["plot_config"] if item["specified_in"] == "python"
-    ]
-    data["plot_options"]["plot_config"] = goal_generator_goals + python_goals
+    if data and "plot_options" in data:
+        goal_generator_goals = [
+            GOAL_TYPE_COMBINED_MODEL[item["goal_type"]](**item)
+            for item in data["plot_options"]["plot_config"]
+            if item["specified_in"] == "goal_generator"
+        ]
+        python_goals = [
+            PlotTableRow(**item) for item in data["plot_options"]["plot_config"] if item["specified_in"] == "python"
+        ]
+        data["plot_options"]["plot_config"] = goal_generator_goals + python_goals
 
     return data

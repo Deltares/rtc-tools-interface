e50e8ca235382074730808eeca29ef066dc416d4
Joris <joris.bentvelsen@vortech.nl>
2023-11-16 16:36:36 +0100

    Generalize the serialization

--- rtctools_interface/optimization/helpers/serialization.py
+++ rtctools_interface/optimization/helpers/serialization.py
@@ -8,8 +8,6 @@ import numpy as np
 import pandas as pd
 from rtctools_interface.optimization.plot_table_schema import PlotTableRow
 
-from rtctools_interface.optimization.type_definitions import PlotDataAndConfig
-
 
 def custom_encoder(obj):
     """Custom JSON encoder for types not supported by default."""
@@ -21,14 +19,16 @@ def custom_encoder(obj):
         return {"__type__": "path", "value": str(obj)}
     if isinstance(obj, pd.DataFrame):
         return {"__type__": "pandas_dataframe", "value": obj.to_json()}
+    if isinstance(obj, PlotTableRow):
+        return {"__type__": "plot_table_row", "value": obj.model_dump()}
     if hasattr(obj, "dict"):
         return obj.dict()
     raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
 
 
-def serialize(plot_data_and_config: PlotDataAndConfig) -> str:
-    """Serialize the PlotDataAndConfig object to a JSON string."""
-    return json.dumps(plot_data_and_config, default=custom_encoder)
+def serialize(object_to_serialize: Any) -> str:
+    """Serialize an object to a JSON string."""
+    return json.dumps(object_to_serialize, default=custom_encoder)
 
 
 def custom_decoder(dct: Any) -> Any:
@@ -47,19 +47,14 @@ def custom_decoder(dct: Any) -> Any:
             return Path(dct["value"])
         if dct.get("__type__") == "pandas_dataframe":
             return pd.read_json(dct["value"])
+        if dct.get("__type__") == "plot_table_row":
+            return PlotTableRow(**dct["value"])
         return dct
     if isinstance(dct, list):
         return [custom_decoder(item) for item in dct]  # Recursively process each item in the list
     return dct
 
 
-def deserialize(serialized_str: str) -> PlotDataAndConfig:
-    """Deserialize the JSON string back to a PlotDataAndConfig object."""
-    data = json.loads(serialized_str, object_hook=custom_decoder)
-
-    # Reconstruct pydantic models
-    if data and "plot_options" in data:
-        plot_table_rows = [PlotTableRow(**item) for item in data["plot_options"]["plot_config"]]
-        data["plot_options"]["plot_config"] = plot_table_rows
-
-    return data
+def deserialize(serialized_str: str) -> dict:
+    """Deserialize the JSON string."""
+    return json.loads(serialized_str, object_hook=custom_decoder)
